b14348ddf7df2ca6a7a85e4bec74244b640d2b73
harutas <96802323+harutas@users.noreply.github.com>
2022-06-18 13:58:49 +0900

    ヘッダーの作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,62 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AppleIcon from '@mui/icons-material/Apple';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { createTheme } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+
+const Header = () => {
+  return (
+    <Box>
+      <AppBar position="static" theme={theme}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="apple"
+            sx={{ mr: 2 }}
+            >
+            <AppleIcon />
+          </IconButton>
+          <Typography variant="h6" component="div">
+            Apple E-Commerce
+          </Typography>
+          <Box sx={{flexGrow: 1}}/>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="favorite"
+            >
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton
+            edge="end"
+            size="large"
+            color="inherit"
+            aria-label="to shopping page"
+            >
+            <ShoppingCartIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: '#f44336',
+    },
+  },
+});
+
+export default Header
